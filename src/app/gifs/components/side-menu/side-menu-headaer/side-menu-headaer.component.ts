293d65972dc807d6bdc106f2142bfa4453eb1950
Anderson Jimenez <andersonjesusujv@hotmail.com>
2025-02-21 12:33:53 -0500

    Add environment configuration and update side menu header with dynamic content

--- src/app/gifs/components/side-menu/side-menu-headaer/side-menu-headaer.component.ts
+++ src/app/gifs/components/side-menu/side-menu-headaer/side-menu-headaer.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { environment } from '@enviroments/environment';
 
 @Component({
   selector: 'gifs-side-menu-headaer',
   templateUrl: './side-menu-headaer.component.html',
 })
-export class SideMenuHeadaerComponent { }
+export class SideMenuHeadaerComponent {
+  envs = environment
+}
