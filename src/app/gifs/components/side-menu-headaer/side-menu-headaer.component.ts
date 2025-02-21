47b15a66b1c99e844b284e1a0c57bebca4b3b3e8
Anderson Jimenez <andersonjesusujv@hotmail.com>
2025-02-21 11:48:18 -0500

    Add side menu components and integrate into dashboard page

---
+++ src/app/gifs/components/side-menu-headaer/side-menu-headaer.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-side-menu-headaer',
+  templateUrl: './side-menu-headaer.component.html',
+})
+export class SideMenuHeadaerComponent { }
---
+++ src/app/gifs/components/side-menu/side-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SideMenuHeadaerComponent } from '../side-menu-headaer/side-menu-headaer.component';
+import { SideMenuOptionsComponent } from '../side-menu-options/side-menu-options.component';
+
+@Component({
+  selector: 'gifs-side-menu',
+  imports: [
+    SideMenuHeadaerComponent,
+    SideMenuOptionsComponent
+  ],
+  templateUrl: './side-menu.component.html',
+})
+export class SideMenuComponent { }
