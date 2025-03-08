f3a175f3d2df4eeeaf107516e546be1ad37dc7cf
Anderson Jimenez <andersonjesusujv@hotmail.com>
2025-03-08 09:49:27 -0500

    Add Giphy API integration and create service for fetching trending GIFs

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,26 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '@enviroments/environment';
+import { GiphyResponse } from '../interfaces/giphy.interface';
+
+@Injectable({providedIn: 'root'})
+export class GifService {
+
+  private http = inject(HttpClient);
+
+  constructor() {
+    this.loadTrendingGifs();
+  }
+
+  loadTrendingGifs() {
+    this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`,
+      {
+        params: {
+          api_key: environment.giphyApiKey,
+          limit: 20,
+        }
+      }
+    )
+  }
+}
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,8 +4,8 @@ export const environment = {
   companyName2: 'App',
   companySlogan: 'Maneja tus gifs',
 
-  // ApiKeys
-
-  //URLS
+  // Giphy API
+  giphyApiKey: 'OL8S4DntNxbSfY7jZ1GV8rp1jq5vDaBv',
+  giphyUrl: 'https://api.giphy.com/v1/gifs',
 
 };
